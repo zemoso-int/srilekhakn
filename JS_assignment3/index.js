faef53dd833e7e5297db0c2738866f59fb7d6dba
srilekha_zomoso <srilekha.kn@zemosolabs.com>
2022-07-11 07:12:42 +0530

    Assignment 3 Object destructured ,template litrales and single line code

---
+++ JS_assignment3/index.js
@@ -0,0 +1,21 @@
+//Single line code
+const printName = (name)=>{return 'Hi '+name}
+
+//Template litrales
+const printBill = (name, bill) => {
+    return `Hi ${name} , please pay: ${bill}`;
+}
+
+const person = {
+     name: 'Noam Chomsky',
+     age: 92
+}
+
+//Object Destriction
+const {name, age} = person;
+
+console.log(name);
+console.log(age);
+
+console.log(printName(name));
+console.log(printBill(name,100));
